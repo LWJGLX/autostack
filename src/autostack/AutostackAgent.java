cc4e83bd059e3ee3cd27879e83fa3a19f1348e10
httpdigest <kburjack@googlemail.com>
2016-03-18 22:45:04 +0100

    No need to recreate stack map frames, because CFG does not join
    We do not change the control-flow graph of a method in such a way that
    it becomes a cyclic graph when it was not already one.
    See:
    http://stackoverflow.com/questions/25109942/is-there-a-better-explanation-of-stack-map-frames
    
    Also, we need not recompute the maxLocals, since it is just original
    maxLocals + 1. The maxStack does not change, because no code we inject
    will require a deeper stack than what was already needed for the
    original method code.

--- src/autostack/AutostackAgent.java
+++ src/autostack/AutostackAgent.java
@@ -18,11 +18,6 @@ import org.objectweb.asm.commons.TryCatchBlockSorter;
 public class AutostackAgent implements Opcodes, ClassFileTransformer {
     private static final String MEMORYSTACK = "org/lwjgl/system/MemoryStack";
     private static final String STACK = "autostack/Stack";
-    private static final boolean AT_LEAST_JAVA7 = atLeastJava7();
-    private static boolean atLeastJava7() {
-        String propValue = System.getProperty("java.class.version");
-        return propValue.startsWith("52.") || propValue.startsWith("51.");
-    }
 
     private static String packageClassPrefix;
 
@@ -77,7 +72,7 @@ public class AutostackAgent implements Opcodes, ClassFileTransformer {
             return null;
 
         // Now, transform all such methods
-        ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS | (AT_LEAST_JAVA7 ? ClassWriter.COMPUTE_FRAMES : 0));
+        ClassWriter cw = new ClassWriter(cr, 0);
         cr.accept(new ClassVisitor(ASM5, cw) {
             public MethodVisitor visitMethod(final int access, String name, final String desc, String signature, String[] exceptions) {
                 final MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
@@ -140,6 +135,10 @@ public class AutostackAgent implements Opcodes, ClassFileTransformer {
                         mv.visitInsn(ATHROW);
                         mv.visitTryCatchBlock(tryLabel, finallyLabel, finallyLabel, "java/lang/Exception");
                         mv.visitEnd();
+                    }
+
+                    public void visitMaxs(int maxStack, int maxLocals) {
+                        mv.visitMaxs(maxStack, maxLocals + 1);
                     }
                 };
                 return own;
