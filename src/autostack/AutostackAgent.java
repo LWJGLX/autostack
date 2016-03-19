da259d04dab5b7a35891079c77ae87ed9dfe213f
httpdigest <kburjack@googlemail.com>
2016-03-19 12:46:41 +0100

    Add license header and JavaDoc

--- src/autostack/AutostackAgent.java
+++ src/autostack/AutostackAgent.java
@@ -1,3 +1,25 @@
+/*
+ * (C) Copyright 2016 Kai Burjack
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+
+ */
 package autostack;
 
 import java.lang.instrument.ClassFileTransformer;
@@ -79,7 +101,7 @@ public class AutostackAgent implements Opcodes, ClassFileTransformer {
                 boolean catches = (info.intValue() & Integer.MIN_VALUE) != 0;
                 if (catches)
                     mv = new TryCatchBlockSorter(mv, access, name, desc, signature, exceptions);
-                MethodVisitor own = new MethodVisitor(ASM5, mv) {
+                mv = new MethodVisitor(ASM5, mv) {
                     Label tryLabel = new Label();
                     Label finallyLabel = new Label();
 
@@ -132,7 +154,7 @@ public class AutostackAgent implements Opcodes, ClassFileTransformer {
                         mv.visitMaxs(maxStack, maxLocals + 1);
                     }
                 };
-                return own;
+                return mv;
             }
         }, 0);
         byte[] arr = cw.toByteArray();
